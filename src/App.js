90b1af1681ff980553249bc074f468ea581890d0
RahulGrayopus <rahul.pandey@grayopus.com>
2024-09-11 17:58:31 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-3xl font-bold text-green-800">Hello netflix gpt </p>
+    </div>
+  );
+}
+
+export default App;
