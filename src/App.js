d908ad5d894f2e3ca18e8954f9408a4233cec41e
RahulGrayopus <rahul.pandey@grayopus.com>
2024-09-14 15:56:28 +0530

    header  and login and also sign In and sign Up form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="App">
-      <p className="text-3xl font-bold text-green-800">Hello netflix gpt </p>
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
